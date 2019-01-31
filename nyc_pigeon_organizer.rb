d4cf3b50ae004e56bd8bba610d7d1b0c7ad603f0
Emily Wagner <emilywagner598@gmail.com>
2019-01-31 01:23:13 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  results = {}
+
+  data.each do |k, v|
+    v.each do |element|
+      element[1].each do |name|
+        if results.keys.include?(name) == false
+          results[name] = {k => [element[0].to_s]}
+        else
+          if results[name].keys.include?(k) == false
+            results[name][k] = [element[0].to_s]
+          else
+          results[name][k] << element[0].to_s
+          end
+        end
+      end
+    end
+  end
+
+
+
+  results
 end
