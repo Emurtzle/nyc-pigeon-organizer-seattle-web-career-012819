b05b28d9bb351998e86c5a8a43c6c199e717318f
Emily Wagner <emilywagner598@gmail.com>
2019-01-31 01:02:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,6 +9,7 @@ def nyc_pigeon_organizer(data)
         if results.keys.include?(name) == false
           results[name] = {k => [element[0].to_s]}
         else
+          binding.pry
           #results[name][k] << element[0].to_s
         end
       end
